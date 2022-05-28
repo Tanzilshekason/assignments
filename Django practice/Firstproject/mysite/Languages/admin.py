7f006fc5644f9026eb003dc4a4f643b4494ce55d
Tanzil <shekasontanzil69@gmail.com>
2022-05-28 18:00:21 +0530

    Django Updated

---
+++ Django practice/Firstproject/mysite/Languages/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Programmer,Language
+
+admin.site.register(Programmer)
+admin.site.register(Language)
+
--- Django practice/Firstproject/mysite/polls/admin.py
+++ Django practice/Firstproject/mysite/polls/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Article
 
-# Register your models here.
+admin.site.site_header = 'POLLS ADMIN'
+admin.site.site_title = 'POLLS ADMIN PORTAL'
+admin.site.index_title = 'WELCOME TO POLLS ADMIN PORTAL'
+
+admin.site.register(Article)
+
+
+class ArticleAdminSite(admin.ModelAdmin):
+    model = Article
+    fields = ['title', 'slug', 'body', 'date']
+    list_display = ('title', 'slug', 'body', 'date')
+    actions = ['description']
+
+def description(self, request, queryset):
+    queryset.update(is_good=False)
+
+
+    admin.site.register(Article.ArticleAdminSite)
